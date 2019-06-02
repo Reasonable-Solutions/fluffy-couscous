e1a5e419d0e75c51dad81d6953d158b7f4b828a1
carl <carh@pm.me>
2019-06-02 17:54:03 +0200

    add containers

--- src/Kmeans.hs
+++ src/Kmeans.hs
@@ -1,12 +1,28 @@
 module Kmeans where
 
 import Data.Vector
+import Data.List
+import qualified Data.Map as M
+import Data.Map (Map(..))
 
 kMeans :: (Vector v, Vectorizable e v)
-  => Int
+  => (Int -> [e] -> [v])
   -- ^ number of centroids
   -> [e]
   -- ^ the data
   -> [v]
   -- ^ centroids after convergence
 kMeans = undefined
+
+clusterAssignment :: (Ord v, Vector v, Vectorizable e v)
+  => [v] -> [e] -> Map v [e]
+clusterAssignment centroids points =
+  let initialMap = M.fromList $ zip centroids (repeat [])
+  in foldr (\p m ->
+             let
+               chosenC = minimumBy (compareDistance p) centroids
+             in M.adjust (p:) chosenC m)
+     initialMap points
+  where compareDistance p x y = compare
+          (distance x $ toVec p)
+          (distance y $ toVec p)
