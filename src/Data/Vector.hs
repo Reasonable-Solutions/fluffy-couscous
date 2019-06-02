b8ceb592d5cc9ebec496b0810cab09a2e27c6cf7
carl <carh@pm.me>
2019-06-02 18:57:01 +0200

    add a convenient type alias

--- src/Data/Vector.hs
+++ src/Data/Vector.hs
@@ -5,6 +5,7 @@
 {-# Language MultiParamTypeClasses #-}
 module Data.Vector where
 
+type V2 = (Double, Double)
 class (Ord v) => Vector (v :: *) where
   distance :: v -> v -> Double
   centroid :: [v] -> v
@@ -12,9 +13,18 @@ class (Ord v) => Vector (v :: *) where
 class Vector v => Vectorizable e v where
   toVec :: e -> v
 
-instance Vector (Double, Double) where
+instance Vector V2 where
+  distance :: V2 -> V2 -> Double
   distance (x1, y1) (x2, y2) = sqrt $ ((x2-x1)**2.0) + ((y2 - y1)**2.0)
+
+  centroid :: [V2] -> V2
   centroid vs = let
-    (sumx, sumy) = foldl (\(x,y) (x0,y0)-> ((x+x0), (y+y0))) (0.0,0.0) vs
+    (sumx, sumy) = foldl (\(x,y) (x0,y0)-> (x+x0, y+y0)) (0.0,0.0) vs
     len = fromIntegral . length $ vs
     in (sumx / len, sumy / len)
+
+ -- >>> distance (0.0,0.0) (1.0,1.0)
+-- 1.4142135623730951
+
+ -- >>> centroid [(110.0,0.0):: V2, (1.0,1.0), (0.0,1.0), (1.0,0.0)]
+-- (28.0,0.5)
--- src/Kmeans.hs
+++ src/Kmeans.hs
@@ -1,3 +1,4 @@
+{-# ConstraintKinds #-}
 module Kmeans where
 
 import Data.Vector
@@ -5,16 +6,9 @@ import Data.List
 import qualified Data.Map as M
 import Data.Map (Map(..))
 
-kMeans :: (Vector v, Vectorizable e v)
-  => (Int -> [e] -> [v])
-  -- ^ number of centroids
-  -> [e]
-  -- ^ the data
-  -> [v]
-  -- ^ centroids after convergence
-kMeans = undefined
+type Vvv e v= (Vector v, Vectorizable e v)
 
-clusterAssignment :: (Ord v, Vector v, Vectorizable e v)
+clusterAssignment :: (Ord v, Vvv e v)
   => [v] -> [e] -> Map v [e]
 clusterAssignment centroids points =
   let initialMap = M.fromList $ zip centroids (repeat [])
@@ -26,3 +20,31 @@ clusterAssignment centroids points =
   where compareDistance p x y = compare
           (distance x $ toVec p)
           (distance y $ toVec p)
+
+refineCentroids :: (Vvv e v)
+  => Map v [e]
+  -- ^ a map of vectors to data
+  -> [(v, v)]
+refineCentroids = M.toList . fmap (centroid . fmap toVector)
+
+shouldStop :: (Vvv e v) => [(v,v)] -> Double -> Bool
+shouldStop centroids threshold = foldl (\(x, y) s -> s + distance x y) 0.0 centroids < threshold
+
+kMeans :: (Vvv e v )
+  => (Int -> [e] -> [v]) -- ^ init function
+  -> Int -- ^ number of centroids
+  -> [e] -- ^ data
+  -> Double -- ^ threshold
+  -> [v] -- ^ final centroids
+kMeans init k es = kmeans' (i k points) points
+
+kmeans' :: (Vvv e v)
+  => [v] -> [e] -> Double -> [v]
+kmeans' centroids points th =
+  let
+    assignments = clusterAssignment centroids points
+    oldNewCentroids = refineCentroids assignments
+    newCentroids = snd <$> oldNewCentroids
+  in case shouldStop oldNewCentroids th of
+    True -> newCentroids
+    False -> kmeans' newCentroids points th
