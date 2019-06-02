b3549958fc0c9a3fa14398ce1deebf36928ccaae
carl <carh@pm.me>
2019-06-02 17:52:49 +0200

    jfc, carl get your stuff together
    I cannot be trusted with anything that relies correct order of params
    nor anything that relies on the right subscripts

--- src/Data/Vector.hs
+++ src/Data/Vector.hs
@@ -1,16 +1,20 @@
 {-# Language FlexibleInstances     #-}
+{-# Language FlexibleContexts      #-}
 {-# Language InstanceSigs          #-}
 {-# Language KindSignatures        #-}
 {-# Language MultiParamTypeClasses #-}
 module Data.Vector where
 
-class Vector (v :: *) where
+class (Ord v) => Vector (v :: *) where
   distance :: v -> v -> Double
+  centroid :: [v] -> v
 
 class Vector v => Vectorizable e v where
   toVec :: e -> v
-  
-instance Vector (Double, Double) where
-  distance :: (Double, Double) -> (Double, Double) -> Double
-  distance (x1, y1) (x2, y2) = sqrt $ (x2-x1)**2.0 * (y2 - x2)**2.0
 
+instance Vector (Double, Double) where
+  distance (x1, y1) (x2, y2) = sqrt $ ((x2-x1)**2.0) + ((y2 - y1)**2.0)
+  centroid vs = let
+    (sumx, sumy) = foldl (\(x,y) (x0,y0)-> ((x+x0), (y+y0))) (0.0,0.0) vs
+    len = fromIntegral . length $ vs
+    in (sumx / len, sumy / len)
